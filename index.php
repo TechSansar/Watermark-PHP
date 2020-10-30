f1bbcd03a2810cc993c9e54378f05ec6f81a0c42
Aashish Parajuli <60426026+asis-parajuli@users.noreply.github.com>
2020-10-30 19:43:20 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
+<?php
 
+$logo = imagecreatefrompng('logo.png'); 
+$image = imagecreatefrompng('image.png');
+
+$margin_right = 500; 
+$margin_bottom = 350; 
+$img_width = imagesx($logo); 
+$img_height = imagesy($logo);
+
+imagecopy($image, $logo, imagesx($image ) - $img_width - $margin_right, imagesy($image) - $img_height - $margin_bottom, 0, 0, imagesx($logo), imagesy($logo));
+
+header('Content-type: image/png'); 
+imagepng($image); 
+imagedestroy($image);
+?>
